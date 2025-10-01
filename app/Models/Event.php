0bdfaf4600b02cbfa05bbb35e325194fc88074d9
Jose Cabalquinto <josegenelidojrc@gmail.com>
2025-10-02 00:59:29 +0800

    Add Event model

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'date', 'location', 'max_participants'
+    ];
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
+}
